4ab12e615aee7dd921e903a13b2d8072bf7ee56d
Carlos Steeven Jaramillo <carlosjaramillo@percepcion.digital>
2020-08-17 21:36:48 -0500

    Limpieza de comentarios basura

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/task', (req, res) => {
-    //res.send('<h1>Soy en Index de las tareas</h1>')
     res.send(TaskModel.findAll());
 })
 
 app.get('/task/:id', (req, response) => {
-    //res.send('<h3>Soy el VIEW de la tarea específica</h3><h1>' + req.params.id + "</h1>")
     response.send(TaskModel.findById(req.params.id));
 })
 
@@ -27,7 +25,6 @@ app.post('/task', (req, res) => {
 })
 
 app.delete('/task/:id', (req, res) => {
-    //res.send('<h3>Soy el DELETE de la tarea específica</h3><h1>' + req.params.id + "</h1>")
     let deleted = TaskModel.deleteById(req.params.id);
     res.send("Res:" + deleted)
 })
